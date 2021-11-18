8bea434728fae0d51eb7215f9b57f9bcde31803e
PuzovSergei <s_puzov@mail.ru>
2021-11-18 15:09:31 +0500

    Task4

--- src/hw15112021/HomeWorkApp.java
+++ src/hw15112021/HomeWorkApp.java
@@ -5,6 +5,7 @@ public class HomeWorkApp {
 
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -19,6 +20,16 @@ public class HomeWorkApp {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 101;
+        if (value <=0) {
+            System.out.println("Красный");
+        } else if (value >0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
         }
     }
 }
